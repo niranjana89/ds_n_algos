ad49a37e1697e4809f52b29d3147d942ea89d77d
niranjana89 <30704985+niranjana89@users.noreply.github.com>
2021-01-01 19:43:20 +0530

    Update simple_queue.py

--- data_structures/python/queue/simple_queue.py
+++ data_structures/python/queue/simple_queue.py
@@ -1,1 +1,37 @@
 
+#queue
+
+class queue():
+    def __init__(self):
+        self.items = []
+    
+    def enqueue(self, item):
+        self.items.append(item)
+        
+    def dequeue(self):
+        if not self.isEmpty():
+            self.items.pop(0)
+    
+    def isEmpty(self):
+        if len(self.items)==0:
+            return True
+        return False
+    
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[len(self.items)-1]
+        return None
+        
+
+q = queue()
+q.enqueue(5)
+q.enqueue(15)
+q.enqueue(25)
+
+print(q.peek())
+q.dequeue()
+q.dequeue()
+print(q.peek())
+q.dequeue()
+q.dequeue()
+print(q.peek())
