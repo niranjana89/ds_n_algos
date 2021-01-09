f744627bc0bab9abc44915991139449589359bc0
niranjana89 <30704985+niranjana89@users.noreply.github.com>
2021-01-09 22:25:22 +0530

    Update linear_search.py

--- algos/searching_algos/linear_search.py
+++ algos/searching_algos/linear_search.py
@@ -13,3 +13,21 @@ def findByLinearSearch(a, k):
 a = [3,10,40,50,5,7,9,18]
 k = 25
 print(findByLinearSearch(a,k))
+
+#return index
+
+#searching algorithms
+#linear search
+
+#complexity is O(n)
+def findIndexByLinearSearch(a, k):
+    for i in range(0, len(a)):
+        if k==a[i]:
+            return i
+    return None
+
+
+
+a = [3,10,40,50,5,7,9,18]
+k = 1
+print(findIndexByLinearSearch(a,k))
