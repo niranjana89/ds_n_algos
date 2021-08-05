385842f639d212e135cd1e441ea6f8715d44e309
niranjana89 <30704985+niranjana89@users.noreply.github.com>
2021-08-05 20:57:43 +0530

    Update HashTable.java

--- data_structures/java/HashTable.java
+++ data_structures/java/HashTable.java
@@ -4,37 +4,38 @@ public class HashTable {
 		String value;
 		ListNode nxt;
 	}
-	
+
 	private ListNode[] tbl;
-	
+
 	private int count;
-	
+
 	public HashTable() {
 		tbl = new ListNode[64];
 	}
-	
+
 	public HashTable(int initSize) {
-		if(initSize <= 0) 
+		if (initSize <= 0)
 			throw new IllegalArgumentException("initSize is invalid");
 		tbl = new ListNode[initSize];
 	}
-	
+
 	/**
 	 * Add (k,v) to hashtable
+	 * 
 	 * @param key
 	 * @param value
 	 */
 	public void put(String key, String value) {
 		// compute index using key
 		int bucket = hash(key);
-		
+
 		// get if the tbl index
 		ListNode headNode = tbl[bucket];
-		
-		// check if the index has listNode or not and Check if the key already present
+
+		// check if the index has listNode or not and check if the key already present
 		ListNode currentNode = headNode;
-		while(currentNode != null) {
-			if(currentNode.key.equals(key)) {
+		while (currentNode != null) {
+			if (currentNode.key.equals(key)) {
 				currentNode.value = value;
 				count++;
 				return;
@@ -42,28 +43,29 @@ public class HashTable {
 			currentNode = currentNode.nxt;
 		}
 		ListNode node = new ListNode();
-		node.key  = key;
+		node.key = key;
 		node.value = value;
 		node.nxt = headNode;
 		tbl[bucket] = node;
 		count++;
-		System.out.println(count +": "+ node.key);
+		System.out.println(count + ": " + node.key);
 	}
-	
+
 	private int hash(Object key) {
 		return (Math.abs(key.hashCode())) % tbl.length;
 	}
-	
+
 	/**
 	 * Return value if the key is found; else return null;
+	 * 
 	 * @param key
 	 * @return
 	 */
 	public String get(String key) {
 		int bucket = hash(key);
 		ListNode headNode = tbl[bucket];
-		while(headNode != null) {
-			if(headNode.key.equals(key)) {
+		while (headNode != null) {
+			if (headNode.key.equals(key)) {
 				return headNode.value;
 			}
 			headNode = headNode.nxt;
@@ -71,32 +73,33 @@ public class HashTable {
 		// log: key cannot be found
 		return null;
 	}
-	
+
 	/**
 	 * Remove key if present
+	 * 
 	 * @param key
 	 */
 	public void remove(String key) {
 		int bucket = hash(key);
 		ListNode headNode = tbl[bucket];
 		ListNode prevNode = null;
-		while(headNode != null) {
-			if(headNode.key.equals(key)) {
-				if(prevNode != null) {
+		while (headNode != null) {
+			if (headNode.key.equals(key)) {
+				if (prevNode != null) {
 					prevNode.nxt = headNode.nxt;
 				} else {
 					prevNode = headNode.nxt;
 					tbl[bucket] = prevNode;
 				}
 				headNode = null;
-				count --;
+				count--;
 				return;
 			}
 			prevNode = headNode;
 			headNode = headNode.nxt;
 		}
 	}
-	
+
 	/**
 	 * returns the count of (k,v) pairs
 	 * 
@@ -104,7 +107,7 @@ public class HashTable {
 	public int count() {
 		return count;
 	}
-	
+
 	// Driver class to test HashTable - get, add, remove, size, getAllItems
 	public static void main(String[] args) {
 		HashTable tbl = new HashTable();
@@ -121,5 +124,5 @@ public class HashTable {
 		System.out.println(tbl.count());
 		System.out.println(tbl.get("hello"));
 	}
-	
+
 }
